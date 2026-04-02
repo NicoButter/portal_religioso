64bcd5bbfb585c7d86fb3830893e54b8fe41fdb9
Nicolás Butterfield <nicobutter@gmail.com>
2026-04-01 21:06:04 -0300

    feat: enhance ChordDiagram interaction with hover effects and click functionality

--- app/transposer/page.tsx
+++ app/transposer/page.tsx
@@ -86,7 +86,13 @@ export default function TransposerPage() {
                     >
                       <ChordDiagram name={chord} />
                       {isSelected && (
-                        <div className="absolute -top-3 -right-3 bg-red-500 text-white rounded-full p-1 shadow-md border-2 border-white pointer-events-none z-10 transition-all">
+                        <div 
+                          className="absolute -top-3 -right-3 bg-red-500 text-white rounded-full p-1 shadow-md border-2 border-white z-10 transition-all hover:bg-red-600 active:scale-90"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            addChord(chord);
+                          }}
+                        >
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                           </svg>
@@ -117,7 +123,13 @@ export default function TransposerPage() {
                     >
                       <ChordDiagram name={chord} />
                       {isSelected && (
-                        <div className="absolute -top-3 -right-3 bg-red-500 text-white rounded-full p-1 shadow-md border-2 border-white pointer-events-none z-10 transition-all">
+                        <div 
+                          className="absolute -top-3 -right-3 bg-red-500 text-white rounded-full p-1 shadow-md border-2 border-white z-10 transition-all hover:bg-red-600 active:scale-90"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            addChord(chord);
+                          }}
+                        >
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                           </svg>
